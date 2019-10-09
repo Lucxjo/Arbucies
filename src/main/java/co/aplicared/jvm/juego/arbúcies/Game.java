865bf9ea80dd79d9a9f1b71c836c75389d162e41
Louis Hollingworth <louis@aplicared.co>
2019-10-09 20:41:30 +0100

    Created the window for the game.
    Signed-off-by: Louis Hollingworth <louis@aplicared.co>

--- src/main/java/co/aplicared/jvm/juego/arbúcies/Game.java
+++ src/main/java/co/aplicared/jvm/juego/arbúcies/Game.java
@@ -8,10 +8,30 @@ public class Game extends Canvas implements Runnable {
     public static int height = width / 16 * 9;
     public static int scale = 3;
 
+    public final static String title = "Arbúcies";
+
     private Thread gameThread;
     private JFrame frame;
 
     private boolean isRunning = false;
+
+    public Game() {
+        Dimension size = new Dimension(width * scale, height * scale);
+        frame = new JFrame();
+        frame.setPreferredSize(size);
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.frame.setResizable(false);
+        game.frame.setTitle(title);
+        game.frame.add(game);
+        game.frame.pack();
+        game.frame.requestFocus();
+        game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.frame.setLocationRelativeTo(null);
+        game.frame.setVisible(true);
+    }
 
     public synchronized void start() {
         gameThread = new Thread(this, "Arbúcies Game Thread");
