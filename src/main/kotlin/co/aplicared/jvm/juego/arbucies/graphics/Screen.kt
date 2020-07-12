909cdb6880d8f320198e6dc428441fd696565328
Louis Hollingworth <louis@aplicared.co>
2020-07-12 20:49:34 +0100

    Now rendering pretty random tiles.
    Signed-off-by: Ludoviko <louis@aplicared.co>

--- src/main/kotlin/co/aplicared/jvm/juego/arbucies/graphics/Screen.kt
+++ src/main/kotlin/co/aplicared/jvm/juego/arbucies/graphics/Screen.kt
@@ -4,14 +4,26 @@
 
 package co.aplicared.jvm.juego.arbucies.graphics
 
+import kotlin.random.Random
+
 class Screen(private val width: Int, private val height: Int) {
     var pixels = IntArray(width * height)
+    var tiles = IntArray(64 * 64)
+    
+    private val random = Random
+    
+    init {
+        for (i in 0 until 64 * 64) {
+            tiles[i] = random.nextInt(0xffffff)
+        }
+    }
     
     fun render() {
         for (y in 0 until height) {
             for (x in 0 until width) {
-                if (y >= height || x >= width) break
-                pixels[x + y * width] = 0xff00ff
+                if (y >= height || x >= width || y < 0 || x < 0) break
+                val tileIndex = (x shr 4) + (y shr 4) * 64
+                pixels[x + y * width] = tiles[tileIndex]
             }
         }
     }
