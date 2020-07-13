057a2189b444ff1d76862761dbf760fb71db55e7
Louis Hollingworth <louis@aplicared.co>
2020-07-13 17:10:38 +0100

    Added Spritesheet handler. Just need to load and test. Funding has been updated.
    Signed-off-by: Ludoviko <louis@aplicared.co>

---
+++ src/main/kotlin/co/aplicared/jvm/juego/arbucies/graphics/Spritesheet.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020 Ludoviko (Louis Hollingworth). This file is subject to the GNU GPL V3.0
+ */
+
+package co.aplicared.jvm.juego.arbucies.graphics
+
+import java.io.IOException
+import javax.imageio.ImageIO
+
+class Spritesheet(private var path: String, private val size: Int) {
+    var pixels = IntArray(size * size)
+    
+    init {
+        load()
+    }
+    
+    private fun load() {
+        try {
+            val image = ImageIO.read(Spritesheet::class.java.getResource(path))
+            val w = image.width
+            val h = image.height
+            image.getRGB(0, 0, w, h, pixels, 0, w)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+    
+}
