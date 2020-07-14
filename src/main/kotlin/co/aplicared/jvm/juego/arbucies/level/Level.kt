f5a0f11c607663521cd41f4ea7d15d748f287414
Louis Hollingworth <louis@aplicared.co>
2020-07-14 14:00:15 +0100

    Added basis of Random Level generation.
    Signed-off-by: Ludoviko <louis@aplicared.co>

--- src/main/kotlin/co/aplicared/jvm/juego/arbucies/level/Level.kt
+++ src/main/kotlin/co/aplicared/jvm/juego/arbucies/level/Level.kt
@@ -6,10 +6,10 @@ package co.aplicared.jvm.juego.arbucies.level
 
 import co.aplicared.jvm.juego.arbucies.graphics.Screen
 
-class Level {
-    private var width: Int? = null
-    private var height: Int? = null
-    private var tiles: IntArray? = null
+open class Level {
+    protected var width: Int? = null
+    protected var height: Int? = null
+    protected var tiles: IntArray? = null
     
     constructor(width: Int, height: Int) {
         this.width = width
@@ -18,26 +18,19 @@ class Level {
         generateLevel()
     }
     
-    constructor(size: Int) {
-        this.width = size
-        this.height = size
-        tiles = IntArray(size * size)
-        generateLevel()
-    }
-    
     constructor(path: String) {
         loadLevel(path)
     }
     
-    private fun loadLevel(path: String) {
+    protected fun loadLevel(path: String) {
     }
     
-    private fun generateLevel() {
+    protected open fun generateLevel() {
     }
     
     fun update() {}
     
     fun render(xScroll: Int, yScroll: Int, screen: Screen) {}
     
-    private fun time() {}
+    protected fun time() {}
 }
